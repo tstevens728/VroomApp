f07091def8a93e79e1428163be92b0be4349ed4e
Tyler Stevens <tstevens728@yahoo.com>
2025-02-12 20:50:07 -0500

    Completed sign in and sign up setup process

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
+import { Link } from 'expo-router'
+import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-const Home = () => {
-    return (
-        <SafeAreaView>
-            <Text>Home</Text>
-        </SafeAreaView>
-    )
+export default function Page() {
+  const { user } = useUser()
+
+  return (
+    <SafeAreaView>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/sign-in">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
+    </SafeAreaView>
+  )
 }
-
-export default Home;
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
 import { Redirect } from "expo-router";
-import { Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "@clerk/clerk-expo";
 
-import "../global.css"
+import "../global.css";
 
 const Home = () => {
-    return <Redirect href="/(auth)/welcome"/>
-}
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href={"/(root)/(tabs)/home"} />;
+  }
+  return <Redirect href="/(auth)/welcome" />;
+};
 
 export default Home;
